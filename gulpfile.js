8e9a4d9b3570eec102be39b88a48d32e37fbce6a
johanahlin <johan.ahlin@lansforsakringar.se>
2021-04-14 15:22:17 +0200

    added missing icons

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const gulp = require('gulp');
 const clean = require('./src/tasks/clean');
-// const buildData = require('./tasks/build-data');
 const buildSprite = require('./src/tasks/build-sprite');
 const buildSpriteSpecial = require('./src/tasks/build-sprite-special');
 const buildSvgDist = require('./src/tasks/build-svg-dist');
