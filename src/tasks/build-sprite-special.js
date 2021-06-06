12159b104bc446bd95ea4e133ec894a215e83cac
johanahlin <johan.ahlin@lansforsakringar.se>
2021-06-06 20:18:37 +0200

    change name on id's

--- src/tasks/build-sprite-special.js
+++ src/tasks/build-sprite-special.js
@@ -4,19 +4,14 @@ const svgSprite = require('gulp-svg-sprite');
 const svgo = require('gulp-svgo');
 var fs = require('fs');
 var path = require('path');
-var merge = require('merge-stream');
-const svgPath = 'src/svg/special';
-function getFolders(dir) {
-  return fs.readdirSync(dir)
-    .filter(function (file) {
-      return fs.statSync(path.join(dir, file)).isDirectory();
-    });
-}
+
+
 const config = {
   shape: {
     id: {
       generator: function (name) {
         return path.basename(name, '.svg')
+
       }
     }
   },
@@ -26,10 +21,10 @@ const config = {
     symbol: {
       // Activate the defs mode
       dest: '',
+
       bust: false, // Cache busting
       example: false, // Build a page
       sprite: 'icons.svg',
-      prefix: '.%s',
     },
   },
 };
