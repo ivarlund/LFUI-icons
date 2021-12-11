77ce0c2dc0eba8f80886d130bae8c7c67aa52d61
johanahlin <johan.ahlin@lansforsakringar.se>
2021-12-11 14:36:26 +0100

    fix: Icons in special folder should now have correct ID

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ const gulp = require('gulp');
 const clean = require('./src/tasks/clean');
 const buildSprite = require('./src/tasks/build-sprite');
 const buildSpriteSpecial = require('./src/tasks/build-sprite-special');
+const buildSvg = require('./src/tasks/build-svg-dist');
 
 /* Tasks */
 gulp.task('clean', clean);
 gulp.task('build:sprite', buildSprite);
 gulp.task('build:spriteSpecial', buildSpriteSpecial);
-gulp.task('build', ['build:sprite', 'build:spriteSpecial']);
+gulp.task('build:svg', buildSvg);
+gulp.task('build', ['build:sprite', 'build:spriteSpecial', 'build:svg']);
